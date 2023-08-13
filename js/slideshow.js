3b81a9abd9dd1de6c688a8cece7685342e4d03ab
Lazaros Adrian Rudopulos <rudopulos@yahoo.com>
2023-08-13 19:32:00 +0300

    v1
    v1

---
+++ js/slideshow.js
@@ -0,0 +1,24 @@
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide) => {
+    slide.style.display = "none";
+  });
+  slides[index].style.display = "block";
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+
+// Pentru a trece la următorul slide la fiecare 3 secunde
+setInterval(nextSlide, 3000);
